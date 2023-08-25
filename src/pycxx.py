02186c9da6c5f8c29f0fd71e454081fc4b3508c6
matrixbot <shyubhamr@gmail.com>
2023-08-25 14:47:59 +0530

    init

---
+++ src/pycxx.py
@@ -0,0 +1,206 @@
+import os
+from subprocess import Popen, PIPE
+import subprocess
+import re
+import sys
+import inspect
+import ast
+from termcolor import colored
+from .load_function import load_func
+from .utils import get_args, write_src, fcall , prompts, create_dir
+from .create_module import add_func_a
+# TODO : paths 
+python_header = "/usr/include/python3.10"
+
+pybind11_header = "/home/matrix/GSoc/ipyhpx/pybind11/include"
+flags = "-std=c++17"
+
+# source_path = "~/tmp"
+
+
+
+
+if type(sys.stdout).__module__ == 'ipykernel.iostream' and type(sys.stdout).__name__ == 'OutStream':
+    is_jupyter = True
+else:
+    is_jupyter = False
+
+home = os.environ["HOME"]
+print("home = ", home)
+
+def set_flags(f):
+    global flags
+    flags = f   
+
+
+
+class py11:
+
+    def __init__(self, *kargs, **kwargs):
+
+        self.recompile = kwargs.get("recompile", False)
+        self.headers = kwargs.get("headers", [])
+
+        self.module_name = kwargs.get("module_name", False)
+        self.lib_path = kwargs.get("lib_path", False)
+        self.sub_module_name = kwargs.get("sub_module_name", False)
+        
+        fun_decls = ""
+        fun_calls = ""
+        funs_list = []
+
+        if "funs" in kwargs:
+            funs_list += kwargs["funs"]
+
+        if "wrap" in kwargs:
+            wrapper = kwargs["wrap"]
+            assert wrapper is not None
+            funs_list.append(wrapper)
+            self.wrapper = wrapper
+        else:
+            self.wrapper = None
+        path = create_dir(self.lib_path)
+        print(path)
+        new_load_func = load_func.replace("lib_path", f'"{path}"')
+        print(new_load_func)
+        if len(funs_list)>0:
+            fun_decls += new_load_func
+            for f in funs_list:
+                fun_decls += f"typedef {f.rettype}(*{f.fun_name}_type_def)({f.args_decl});\n"
+                fun_decls += f"{f.fun_name}_type_def {f.fun_name} = nullptr;\n"
+                fun_calls += f'{f.fun_name} = ({f.fun_name}_type_def)load_func("{f.fun_name}");\n'
+
+        print("fun_decls = ", fun_decls)
+        print("fun_calls = ", fun_calls)
+
+        self.fun_decls = fun_decls
+        self.fun_calls = fun_calls
+
+    def __call__(self, fun):
+        # Naming the filename and libname
+        # function name module name and its path 
+        if self.lib_path:
+            print(self.lib_path)
+            path = create_dir(self.lib_path)
+            print(path)
+            base = os.path.join(str(path), fun.__name__ )
+            print(base)
+        else:
+            base = os.path.join("~/tmp", fun.__name__)
+            print(base)
+        if not os.path.exists(self.lib_path):
+            print("are we chekcing this")
+            path = create_dir(self.lib_path)
+            os.makedirs(os.path.dirname(path), exist_ok=True)
+        
+
+
+        # AST (Abstract Syntax Tree) Parsing 
+        src = inspect.getsource(fun)
+        tree = ast.parse(src)
+        args, oargs, cargs, rettype = get_args(tree)
+        
+        code = ""
+        fname = base+'.cpp'
+        mname = base+'.so.txt'
+
+        version = self.__get_version(mname)
+        suffix = "_v"+str(version)
+        libname = base+suffix+'.so'
+        
+
+        src = self._call_write_src(fun,rettype,args,oargs,cargs,suffix)
+
+        if os.path.exists(fname):
+            code = open(fname).read()
+        
+        # Recompile case
+        if code != src or self.recompile or not os.path.exists(libname):
+            old_file = base + suffix + '.so'
+            if os.path.exists(old_file):
+                os.remove(old_file)
+            version += 1
+            suffix = "_v"+str(version)
+            base += suffix
+
+            src = self._call_write_src(fun,rettype,args,oargs,cargs,suffix)
+
+            with open(mname, "w") as fd:
+                print(version, file=fd)
+
+            with open(fname, "w") as fd:
+                fd.write(src)
+                
+            if not self.__execute_compilation(base,fname,fun,suffix):
+                if os.path.exists(base+".so"):
+                    os.remove(base+".so")
+                return None
+
+
+        return fcall(base, fun.__name__, suffix, args, rettype,self.lib_path)
+        
+    def _call_write_src(self,fun,rettype,args,oargs,cargs,suffix):
+        return write_src(
+            name=fun.__name__+suffix,
+            raw_name=fun.__name__,
+            wrap_name=fun.__name__,
+            wrapper=self.wrapper,
+            body=fun.__doc__,
+            headers='\n'.join(['#include '+h for h in self.headers]),
+            rettype=rettype,
+            args=args,
+            oargs=oargs,
+            cargs=cargs,
+            fun_decls=self.fun_decls,
+            fun_calls=self.fun_calls
+        )
+    def __get_version(self, mname):
+        if os.path.exists(mname):
+            with open(mname, "r") as fd:
+                return int(fd.read().strip())
+        else:
+            return 0
+
+    def __remove_old_file(self, base, suffix):
+
+        old_file = base + suffix + '.so'
+        if os.path.exists(old_file):
+            os.remove(old_file)
+
+    def __write_new_src(self, base, version, fname, fun, rettype, args, oargs, cargs):
+        version += 1
+        suffix = "_v"+str(version)
+        base += suffix
+        src = self._call_write_src(fun,rettype,args,oargs,cargs,suffix)
+
+        with open(base+'.so.txt', "w") as fd:
+            fd.write(str(version))
+
+        with open(fname, "w") as fd:
+            fd.write(src)
+            
+        return base, suffix
+
+    def __execute_compilation(self, base, fname,fun,suffix):
+        prompts("started executing compilation ....")
+        cmd = "c++ -Wl,--start {flags} -I{python_header} -I{pybind11_header} -rdynamic -fPIC -shared -o {base}.so {fname} -Wl,--end".format(
+                    base=base, python_header=python_header, pybind11_header=pybind11_header, flags=flags, fname=fname)
+
+        try:
+            split_cmd = re.split(r'\s+', cmd)
+            proc = Popen(split_cmd, stdout=PIPE, stderr=PIPE,universal_newlines=True)
+            outs, errs = proc.communicate()
+            if proc.poll() != 0:
+                print(outs) # assume stdout should not be colored
+                print(colored(errs, "red"))  # assuming `colored` is a function
+                return False
+            
+            add_func_a(fun.__name__+suffix, self.lib_path, self.module_name)
+        except Exception as e:
+            print(e)
+            print("Exception raised during compilation")
+            return False
+        prompts("finished executing compilation")
+        return True
+    
+  
---
+++ src/utils.py
@@ -0,0 +1,299 @@
+# utils.py
+import os 
+import re
+import sys
+import inspect
+import ast
+import numpy as np
+import importlib
+from termcolor import colored
+from .load_function import load_func
+from colorama import Fore, Style, init
+from IPython.display import Markdown, display
+
+
+init()
+def prompts(a):
+    display(Markdown(f'<span style="color:red">{a}</span>'))
+
+
+
+
+if type(sys.stdout).__module__ == 'ipykernel.iostream' and type(sys.stdout).__name__ == 'OutStream':
+    is_jupyter = True
+else:
+    is_jupyter = False 
+
+
+class basic_type:
+    def __init__(self, name=None):
+        if name is not None:
+            self.full_name = name
+
+
+class template_type:
+    def __init__(self, name=None):
+        if name is not None:
+            self.full_name = name
+
+    def __getitem__(self, a):
+        pass
+
+    def __getslice__(self, *a):
+        pass
+
+
+type_names = {}
+
+def create_dir(directory_path):
+    full_path = os.path.abspath(directory_path)
+
+    if not os.path.exists(full_path):
+        print("Creating directory:", full_path)
+        os.makedirs(full_path)
+    else:
+        print("Directory already exists:", full_path)
+    return full_path
+
+def create_type(name, alt=None, is_template=False):
+    global type_names
+    assert name is not None
+    if alt is not None:
+        type_names[name] = alt
+    else:
+        type_names[name] = name
+    stack = inspect.stack()
+    if 1 < len(stack):
+        index = 1
+    else:
+        index = 0
+    if is_template:
+        stack[index].frame.f_globals[name] = template_type(name)
+    else:
+        stack[index].frame.f_globals[name] = basic_type(name)
+
+create_type("svec", alt="std::vector", is_template=True)
+create_type("smap", alt="std::map", is_template=True)
+
+
+def gettype(ty):
+    if ty is None:
+        return "None"
+    t = type(ty)
+    if t == ast.Name:
+        if ty.id in type_names.keys():
+            return type_names[ty.id]
+        return ty.id
+    elif t in [np.float64]:
+        return str(t)
+    elif t in [ast.Index, ast.NameConstant]:
+        return gettype(ty.value)
+    elif t in [ast.Attribute]:
+        return gettype(ty.value) + "." + gettype(ty.attr)
+    elif t == ast.Subscript:
+        return gettype(ty.value) + '[' + gettype(ty.slice) + ']'
+    elif t == ast.Tuple:
+        s = ''
+        sep = ''
+        for e in ty.elts:
+            s += sep + gettype(e)
+            sep = ','
+        return s
+    elif t == ast.Call:
+        if ty.func.id == "Ref":
+            return "%s&" % gettype(ty.args[0])
+        elif ty.func.id == "Const":
+            return "%s const" % gettype(ty.args[0])
+        elif ty.func.id == "Move":
+            return "%s&&" % gettype(ty.args[0])
+        elif ty.func.id == "Ptr":
+            return "%s*" % gettype(ty.args[0])
+        else:
+            s = ty.func.id + "<"
+            for i in len(ty.args):
+                if i > 0:
+                    s += ","
+                arg = ty.args[i]
+                s += gettype(arg)
+            s += ">"
+            return s
+        
+    elif type(ty) == str:
+        print("type is str",ty)
+        raise Exception("?")
+    
+    elif type(ty) == ast.Constant:
+        if ty.value is None:
+            return "void"
+        else:
+            raise Exception()
+        
+    else:
+        print(ty.func.id)
+        print(ty.args, "//", dir(ty.args[0]))
+        print(ty.args[0].s, ty.args[1].id)
+        print("<<", ty.__class__.__name__, ">>", dir(ty))
+        raise Exception("?")
+    
+
+# change 1
+def ttran(n):
+    s = ''
+    for match in re.finditer(r'[\w\.]+|.', n):
+        s += {
+            "np.float32": "std::float32_t",
+            "np.float64": "std::float64_t",
+            "np.int64": "std::int64_t",
+            "str": "std::string",
+            "None": "void",
+            "List": "std::vector",
+            "Dict": "std::map",
+            "[": "<",
+            "]": ">"
+        }.get(match.group(0), match.group(0))
+    print("type translation" ,s)
+    return s
+
+def get_args(tree):
+    nm = tree.__class__.__name__
+
+    if nm in ["Module"]:
+        for k in tree.body:
+            args = get_args(k)
+            if args is not None:
+                return args
+
+    elif nm in ["FunctionDef"]:
+        args = []
+        cargs = []
+        oargs = ""
+        for a in tree.args.args:
+            type = ttran(gettype(a.annotation))
+            args += [type+" "+a.arg]
+            oargs += ',py::arg("%s")' % a.arg
+            cargs += [a.arg]
+            #oargs += ','+type
+        return [",".join(args), oargs, cargs, ttran(gettype(tree.returns)) ]
+
+    raise Exception("Could not find args")
+
+class fcall:
+    def __init__(self,base,fun_name,suffix,args,rettype,lib_path):
+        self.base = base
+        self.fun_name = fun_name
+        assert not re.match(r'.*_v\d+$',fun_name), fun_name
+        self.suffix = suffix
+        self.args_decl = args
+        self.rettype = rettype
+        self.lib_path = lib_path
+        # if tmp_dir() not in sys.path:
+        #     sys.path += [tmp_dir()]
+        if lib_path not in sys.path:
+            sys.path += [lib_path]
+        fpath = os.path.join(lib_path,fun_name+suffix+".so")
+        self.m = importlib.import_module(fun_name+suffix,fpath)
+
+    def __call__(self,*args):
+        self.m.load()
+        if is_jupyter:
+            try:
+                outfile = os.path.join(self.lib_path,"out.txt")
+                fd1 = open(outfile, "w")
+                save_out = os.dup(1)
+                os.close(1)
+                os.dup(fd1.fileno())
+
+                errfile = os.path.join(self.lib_path,"err.txt")
+                fd2 = open(errfile, "w")
+                save_err = os.dup(2)
+                os.close(2)
+                os.dup(fd2.fileno())
+
+                return self.m.call(*args)
+            finally:
+                fd1.close()
+                os.close(1)
+                os.dup(save_out)
+                os.close(save_out)
+                print(open(outfile,"r").read(),end='')
+
+                fd2.close()
+                os.close(2)
+                os.dup(save_err)
+                os.close(save_err)
+                print(colored(open(errfile,"r").read(),"red"),end='')
+        else:
+            return self.m.call(*args)
+
+# Create the basic source code
+def write_src(**kwargs):
+
+        wrapper = kwargs['wrapper']
+        raw_name = kwargs["raw_name"]
+        args = kwargs["args"]
+        oargs = kwargs["oargs"]
+        rettype = kwargs["rettype"]
+
+        if wrapper is not None:
+            cargs = kwargs["cargs"]
+            call_args = "(" + ",".join(cargs) + ")"
+            kwargs['wrap'] = wrap_src(
+                f=raw_name,
+                args=args,
+                rettype=rettype,
+                oargs=oargs,
+                call_args=call_args,
+                wrapper=wrapper.fun_name
+            )
+            kwargs['wrap_name'] = raw_name + "_wrapped"
+        else:
+            kwargs['wrap_name'] = raw_name
+            kwargs['wrap'] = ''
+
+        return """
+                #include <pybind11/pybind11.h>
+                #include <pybind11/stl.h>
+                {headers}
+
+                {fun_decls}
+
+                namespace py = pybind11;
+
+                extern "C" {rettype} {raw_name}({args}){{
+                {body}
+                }};
+
+                {wrap}
+
+                extern "C" void load_functions() {{
+                    {fun_calls}
+                }}
+
+                PYBIND11_MODULE({name}, m) {{
+                    m.def("load",load_functions,"function"),
+                    m.def("call",{wrap_name},"function"{oargs});
+                }}""".format(**kwargs)
+
+def wrap_src(**kwargs):
+    if kwargs["rettype"] == "void":
+        src = """
+                void {f}_wrapped({args}) {{
+                    std::function<void()> f = [&](){{
+                        {f}{call_args};
+                    }};
+                    {wrapper}(f);
+                }}""".format(**kwargs)
+        
+    else:
+        src = """
+                {rettype} {f}_wrapped({args}) {{
+                    {rettype} r;
+                    std::function<void()> f = [&](){{
+                        r = {f}{call_args};
+                    }};
+                    {wrapper}(f);
+                    return r;
+                }}""".format(**kwargs)
+        
+    return src
+
