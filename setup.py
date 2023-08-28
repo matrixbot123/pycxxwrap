11cd6a4a3f1afde557044f69629ea9c474ece9a4
matrixbot <shyubhamr@gmail.com>
2023-08-28 17:46:16 +0530

    moved everything into setup.py from pyproject.toml for now

--- setup.py
+++ setup.py
@@ -1,3 +1,25 @@
-import setuptools 
-if __name__ == "__main__": 
-   setuptools.setup()
+from setuptools import setup, find_packages
+
+setup(
+    name="cxxwrap",
+    version="0.0.1",
+    description="Tool used to warpping CXX code in python",
+    long_description=open("README.md", "r").read(),
+    long_description_content_type="text/markdown",
+    author="Shubham Kumar",
+    author_email="shyubhamr@gmail.com",
+    license="Boost Software License 1.0 (BSL-1.0)",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Operating System :: POSIX :: Linux",
+        "License :: OSI Approved :: Boost Software License 1.0 (BSL-1.0)",
+        "Programming Language :: Python :: 3",
+    ],
+    keywords=[
+        "CXX Code warpping",
+        "Custom CXXtypes in python",
+    ],
+    packages=find_packages(),
+    install_requires=["pybind11", "numpy", "IPython", "colorama", "termcolor"],
+    python_requires=">=3.10",
+)
